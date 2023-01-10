c16934db5b2e311c136b6e934f3876932ede9d5f
Diego Arthur <digo.arthur@hotmail.com>
2023-01-10 11:16:17 +0100

    feat: add Header, Cards, Demonstrations and Footer.

--- github-automated-repos-page/src/components/Header/index.jsx
+++ github-automated-repos-page/src/components/Header/index.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import './style.css';
-
+import './style.scss';
+import { FiExternalLink } from 'react-icons/fi';
 export function Header() {
     return (
         <div className="header_Container">
-            <div className="logoHeader">
-                <img src="https://user-images.githubusercontent.com/59892368/211217236-86d5d9d0-8991-4a65-a29c-60bd65621ced.svg" />
-            </div>
-            <div className="navBar">
-                <div className='nav_Options'>
-                    <a>HOME</a> 
-                    <a>DOCUMENTATION</a>
+            <div className="header_Content">
+                <div className="logoHeader">
+                    <img src="https://user-images.githubusercontent.com/59892368/211217236-86d5d9d0-8991-4a65-a29c-60bd65621ced.svg" />
                 </div>
-                <div className="nav_Github">
-                    <a>Github</a>
+                <div className="navBar">
+                    <div className='nav_Options'>
+                        <a href='#'>Home</a>
+                        <a href='#'>Documentation</a>
+                    </div>
+                    <div className="nav_Github">
+                        <a href='#'>
+                           Github
+                            <FiExternalLink/>
+                        </a>
+                      
+                    </div>
                 </div>
             </div>
-
         </div>
     )
 }
