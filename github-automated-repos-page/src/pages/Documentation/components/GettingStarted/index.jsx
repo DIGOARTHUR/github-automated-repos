896c3a26f0c9e4e2cdae314b449ad3b71690e027
Diego Arthur <digo.arthur@hotmail.com>
2023-02-03 23:04:00 +0100

    improvement: adjustments in Import Package (add imgs)

--- github-automated-repos-page/src/pages/Documentation/components/GettingStarted/index.jsx
+++ github-automated-repos-page/src/pages/Documentation/components/GettingStarted/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Highlighter } from "rc-highlight";
-import './style.scss';
+import "./style.scss";
 
-
-const highlighterStyle = { paddingLeft: '5px', height: '60px', backgroundColor: 'rgb(40, 44, 52)' }
-const highlighterStyleFragments = { fontSize: '14px', paddingLeft: '30px', height: '350px', backgroundColor: 'rgb(20, 38, 99)' }
+const highlighterStyle = {
+  paddingLeft: "5px",
+  height: "60px",
+  backgroundColor: "rgb(40, 44, 52)",
+};
+const highlighterStyleFragments = {
+  fontSize: "14px",
+  paddingLeft: "30px",
+  height: "350px",
+  backgroundColor: "rgb(20, 38, 99)",
+};
 const packageInstall = `
     yarn add github-automated-repos
 `;
@@ -14,91 +22,138 @@ const importPackage = `
 `;
 
 export function GettingStarted() {
-
   return (
     <>
-
       <div className="gettingStarted_Container">
         <div className="gettingStarted_Content">
           <h1 className="document_Title">Getting Started</h1>
           <hr />
           <ul className="gettingStarted_Summary">
-            <li >
+            <li>
               <a href="#InstallPackage">Install Package</a>
             </li>
             <li>
               <a href="#ImportPackage"> Import Package</a>
-
             </li>
             <li>
               <a href="#ExampleCode"> Example Code</a>
-
             </li>
           </ul>
           <br />
           <div id="InstallPackage" className="step01_Container">
             <h2> Install Package</h2>
 
-            <Highlighter style={highlighterStyle}>{packageInstall}</ Highlighter>
+            <Highlighter style={highlighterStyle}>{packageInstall}</Highlighter>
           </div>
-
-
 
           <div id="ImportPackage" className="step02_Container">
             <h2>Import Package</h2>
-            <Highlighter style={highlighterStyle}>{importPackage}</ Highlighter>
+            <Highlighter style={highlighterStyle}>{importPackage}</Highlighter>
             <br />
             <h4> The package imports three elements: </h4>
-            <p><code style={{ color: '#60d2ff' }} className="code">useGithubAutomatedRepos</code> hook responsável por automatizar os repositórios. Ele retorna um função chamada <code>dataGithubRepos</code>, que recebe dois parâmetros: data (dado que vem da API) e a palavra-chave (esta última responsável pela aparição do projeto na página a partir do momento em que é declarada no campo Tópicos em seu repositório). O <code>dataGithubRepos</code> retorna, de forma otimizada, uma array de objetos contendo 5 propriedades: id, html_url, topics, name, descriptrion</p>
+            <p>
+              <code style={{ color: "#60d2ff" }} className="code">
+                useGithubAutomatedRepos
+              </code>{" "}
+              hook responsável por automatizar os repositórios. Ele retorna um
+              função chamada <code>dataGithubRepos</code>, que recebe dois
+              parâmetros: data (dado que vem da API do Github) e a palavra-chave
+              (esta última responsável pela aparição do projeto na página a
+              partir do momento em que é declarada no campo Tópicos em seu
+              repositório). O <code>dataGithubRepos</code> retorna, de forma
+              otimizada, uma array de objetos contendo 5 propriedades: id,
+              html_url, topics, name, descriptrion
+            </p>
 
             <ul>
-              <li><b>id:</b> número identificação do repositório. Usado como parâmetro na tag key. </li>
-              <li><b>html_url:</b> link referente ao repositório. Usado como o link de acesso. </li>
-              <li><b>topics:</b> array que traz informações referente aos ícones de Projeto e Stack. Usado nos dois componentes ProjectIcon e StackIcon.</li>
-              <li><b>name:</b> refere-se ao nome dado ao repositório. Usado como título de um card por exemplo. </li>
-              <li><b>description:</b> esta é a descrição dada ao seu repositório. Usado como introdução do seu projeto.</li>
+              <li>
+                <b>id:</b> número identificação do repositório. Usado como
+                parâmetro na tag key. Este campo não precisa ser preenchido.{" "}
+              </li>
+              <li>
+                <b>html_url:</b> link do repositório. Usado como o link de
+                acesso. Este campo não precisa ser preenchido.
+              </li>
+
+              <li>
+                <b>topics:</b> array que traz informações referente aos ícones
+                de Projeto e Stack. Usado nos dois componentes ProjectIcon e
+                StackIcon. E é neste campo que é repassada a chave configurada
+                no hook. Refere-se ao campo <code> About / Topics</code> do seu
+                Github.
+                <img src="https://user-images.githubusercontent.com/59892368/216723905-7c0ad730-052e-4eec-b2e3-94cd55a20e86.png" />
+              </li>
+
+              <li>
+                <b>name:</b> este é o nome do repositório. Refere-se ao campo{" "}
+                <code> Settings / General / Repository name</code> do seu
+                Github.
+                <img src="https://user-images.githubusercontent.com/59892368/216724741-dd5982d9-8b52-4576-beff-6ab1370fac34.png" />
+              </li>
+              <li>
+                <b>description:</b> esta é a descrição dada ao seu repositório.
+                Refere-se ao campo <code>About / Description</code> do seu
+                Github.
+                <img src="https://user-images.githubusercontent.com/59892368/216720448-6ead1097-df1e-4ebf-a98e-b04c00b974ab.png" />
+              </li>
             </ul>
             {/*<br /><br /><Highlighter style={highlighterStyleFragments}>{codeFragments_Hook}</ Highlighter>*/}
 
-
-            <p><code style={{ color: '#5ed49d' }} className="code">ProjectIcon</code> component that renders the project icon, the one that represents the area to which it belongs. Check out the Project Icons tab!</p>
+            <p>
+              <code style={{ color: "#5ed49d" }} className="code">
+                ProjectIcon
+              </code>{" "}
+              component that renders the project icon, the one that represents
+              the area to which it belongs. Check out the Project Icons tab!
+            </p>
             {/* <br /><br /><Highlighter style={highlighterStyleFragments}>{codeFragments_ProjectIcon}</ Highlighter>*/}
 
-
-            <p><code style={{ color: '#5ed49d' }} className="code">StackIcon</code> component that renders the icons of the stacks used in your project. Check out the Stack Icons tab!</p>
+            <p>
+              <code style={{ color: "#5ed49d" }} className="code">
+                StackIcon
+              </code>{" "}
+              component that renders the icons of the stacks used in your
+              project. Check out the Stack Icons tab!
+            </p>
             {/*<br /><br /><Highlighter style={highlighterStyleFragments}>{codeFragments_StackIcons}</ Highlighter>*/}
 
-            <p><code style={{ color: '#5ed49d' }} className="code">IGithubRepos</code> interface para o projeto em Typescript. Usado para tipar o useState que receberá a array.</p>
+            <p>
+              <code style={{ color: "#5ed49d" }} className="code">
+                IGithubRepos
+              </code>{" "}
+              interface para o projeto em Typescript. Usado para tipar o
+              useState que receberá a array.
+            </p>
           </div>
 
           <div id="ExampleCode" className="step03_Container">
-
             <h2>Code Example</h2>
             <div className="jsx_Content">
-              <img className="jsxIcon" src="https://user-images.githubusercontent.com/59892368/210763677-ee1a0283-eea7-45de-9589-86d7c50a8cb0.svg" />
+              <img
+                className="jsxIcon"
+                src="https://user-images.githubusercontent.com/59892368/210763677-ee1a0283-eea7-45de-9589-86d7c50a8cb0.svg"
+              />
               <h3>.JSX</h3>
             </div>
-            <Highlighter >{codeExampleJSX}</ Highlighter>
+            <Highlighter>{codeExampleJSX}</Highlighter>
             <br />
 
-
             <div className="tsx_Container">
-              <img className="tsxIcon" src="https://user-images.githubusercontent.com/59892368/211927874-fb9b60ed-64dd-4091-9880-62af2a2206a1.svg" />
+              <img
+                className="tsxIcon"
+                src="https://user-images.githubusercontent.com/59892368/211927874-fb9b60ed-64dd-4091-9880-62af2a2206a1.svg"
+              />
               <h3>.TSX</h3>
             </div>
-            <Highlighter >{codeExampleTSX}</ Highlighter>
-
+            <Highlighter>{codeExampleTSX}</Highlighter>
           </div>
         </div>
       </div>
-
     </>
-
-  )
+  );
 }
 
-const codeFragments_Hook =
-  `const { dataReposGithub } = useGithubAutomatedRepos()
+const codeFragments_Hook = `const { dataReposGithub } = useGithubAutomatedRepos()
 const [repository, setRepository] = useState([])
 useEffect(() => {
                             {/*Put here your github Name*/}
@@ -113,7 +168,7 @@ useEffect(() => {
                                     id: number;    
                                 */}  
 }, [])
-`
+`;
 
 const codeFragments_ProjectIcon = `
 {
@@ -141,7 +196,7 @@ const codeFragments_ProjectIcon = `
     })
 }
 ...
-`
+`;
 
 const codeFragments_StackIcons = `
 {
@@ -167,11 +222,11 @@ const codeFragments_StackIcons = `
     )
     })
 }
-`
+`;
 
 const codeFragments_04 = `
 import { useGithubAutomatedRepos, IconProjects , IconSkill} from 'github-automated-repos'; {/*Import Package*/}
-`
+`;
 
 const codeExampleJSX = `   
   import './App.css';
@@ -230,8 +285,6 @@ const codeExampleJSX = `
 
 
 `;
-
-
 
 const codeExampleTSX = `
     import { useEffect, useState } from 'react';
