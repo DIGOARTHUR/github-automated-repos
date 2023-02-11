dd8bc380fd9481dfd59f17fcecbb87e95e1be54e
Diego Arthur <digo.arthur@hotmail.com>
2023-02-11 19:58:14 +0100

    refactor: refresh package github-automated-repos in page

--- github-automated-repos-page/.yalc/github-automated-repos/StackIcon.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-type Props = {
-    iconItem: string;
-    className?: string;
-};
-export declare function StackIcon({ iconItem, className }: Props): JSX.Element;
-export {};
--- github-automated-repos-page/.yalc/github-automated-repos/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { ProjectIcon } from './ProjectIcon';
-export { StackIcon } from './StackIcon';
-export { IGithubRepos, useGithubAutomatedRepos, IconsData } from './hooks/useGithubAutomatedRepos';
