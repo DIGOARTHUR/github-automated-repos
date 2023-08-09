dbe8bfa91ebac70d402fad9bf57e7f9ec3e920e4
digoarthur <digo.arthur@hotmail.com>
2023-08-09 23:09:01 +0100

    feat(Footer,Header): add Footer and Header component

---
+++ github-automated-repos-page/new/src/app/components/Footer/index.tsx
@@ -0,0 +1,46 @@
+
+import Image from "next/image";
+import npm_icon from "../../../../public/imgs/npm_icon.svg";
+import github_icon from "../../../../public/imgs/github_icon.svg";
+export default function Footer() {
+    return (
+        <footer className="w-full bg-gradient-to-r from-[#31313E] to-[#3e3e4b] font-sans pb-10">
+            <div className="grid grid-cols-2  xl:grid-cols-4 gap-x-12 gap-y-8 max-w-7xl mx-auto p-10">
+
+
+                <div className="flex flex-col gap-2">
+                    <div className=" text-lg">
+                        @digoarthur
+                    </div>
+                    <div className="font-mono">
+                        ©2023
+                    </div>
+                    <div>
+                    Based in:
+                    API GitHub
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <div  className="font-bold text-xl">Getting Start</div>
+                    <div>Install package</div>
+                    <div>Import Package</div>
+                    <div>Fill in the fields</div>
+                    <div>Code Example</div>
+
+                </div>
+                <div className="flex flex-col gap-2">
+                    <div className="font-bold text-xl">Icons</div>
+                    <div>Stacks</div>
+                    <div>Projects</div>
+
+                </div>
+                <div className="flex flex-col gap-2">
+                   <div className="font-bold text-xl">More</div>
+                   <div className="flex gap-1.5 ">    <Image src={github_icon} alt='npm_icon' /> <span >GitHub</span></div>
+                 
+                   <div className="flex gap-1.5 ">    <Image src={npm_icon} alt='npm_icon' /> <span >NPM</span></div>
+                </div>
+            </div>
+        </footer>
+    )
+}
---
+++ github-automated-repos-page/new/src/app/components/Header/index.tsx
@@ -0,0 +1,31 @@
+
+import Image from "next/image";
+import logoLib from "../../../../public/logoLib.svg";
+import npm_icon from "../../../../public/imgs/npm_icon.svg";
+import github_icon from "../../../../public/imgs/github_icon.svg";
+export default function Header() {
+    return (
+        <nav className="w-full flex justify-center items-center  font-sans h-16 fixed top-0 left-0 bg-[#3d3d4dab] z-40 ">
+            <div className="flex w-full items-center justify-between mx-10 ">
+                <div className=" ">
+                    <Image className="h-14 w-14" src={logoLib} alt='logoLib' />
+
+                </div>
+
+                <div className=" hidden w-full  h-10 px-4">
+                    <button className=" bg-[#4F4F60] w-full h-full flex items-center rounded-full p-4">
+                        <Image className="h-4 w-4 mr-3" src={logoLib} alt='logoLib' />
+                      Search
+
+                    </button>
+                </div>
+
+                <div className="flex gap-3">
+                <Image className="h-5 w-5" src={npm_icon} alt='npm_icon' />
+                <Image className="h-5 w-5" src={github_icon} alt='npm_icon' />
+                </div>
+            </div>
+
+        </nav >
+    )
+}
